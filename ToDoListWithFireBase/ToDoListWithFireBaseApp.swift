f7311a4778b524a9f4db21d5ba9018a9725d1bb4
DhaiAssaf <dhasaf@icloud.com>
2024-04-27 22:49:11 +0300

    Initial Commit

---
+++ ToDoListWithFireBase/ToDoListWithFireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListWithFireBaseApp.swift
+//  ToDoListWithFireBase
+//
+//  Created by Dhai Alassaf on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListWithFireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
